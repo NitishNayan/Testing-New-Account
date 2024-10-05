3a223d2e52e134cc5552dc74330a97c70df7aeef
Nitish Nayan <Nitish Nayan@DESKTOP-OECPI9S>
2024-10-05 12:57:00 +0530

    2

--- src/test/java/TEST/test.java
+++ src/test/java/TEST/test.java
@@ -17,5 +17,6 @@ public class test
 	driver.manage().window().maximize();
 	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 	driver.get("https://demowebshop.tricentis.com/");	
+	//aaaaaaaaaaaaaaaaaa
 	}
 }
